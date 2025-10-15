6bbc20ef7382c96831ef26ab2c1f6cd55f8a6707
levelstage <149927847+levelstage@users.noreply.github.com>
2025-10-15 11:11:18 +0000

    private로 되어있던 멤버들을 public으로 바꾸었습니다.

--- GfToolkit.Shared/Behaviors/AreaAttackBehavior.cs
+++ GfToolkit.Shared/Behaviors/AreaAttackBehavior.cs
@@ -1,14 +1,13 @@
 using GfToolkit.Shared.Battles;
-using GfToolkit.Shared.Core;
 
 namespace GfToolkit.Shared.Behaviors
 {
     // '범위 공격'이라는 메커니즘을 책임지는 클래스
     public class AreaAttackBehavior : Behavior
     {
-        private int Power;
-        private DamageType DamageType;
-        private PatternSet Area;
+        public int Power;
+        public DamageType DamageType;
+        public PatternSet Area;
 
         // 생성자에서 피해량, 피해 타입, 공격 범위(PatternSet) 등 '데이터'를 받는다.
         public AreaAttackBehavior() : base()
@@ -26,7 +25,7 @@ namespace GfToolkit.Shared.Behaviors
                 if (bt.Type == TargetType.Accessible) // 공격 가능한 칸에 있는 유닛에게만 피해
                 {
                     Square sq = map[bt.Y, bt.X];
-                    if (sq.Occupant != null && Accessible.Contains(GameData.GetTeamType(origin.Occupant.Team, sq.Occupant.Team)))
+                    if (sq.Occupant != null)
                     {
                         sq.Occupant.TakeDamage(Power, DamageType);
                     }
